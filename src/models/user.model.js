95cc646c56d6cf335a1538f048a4e15f50c70c0e
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-05-02 16:39:42 +0530

    Database setup usermodel

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"username already exists"],
+        required:true,
+    },
+
+    email:{
+        type:String,
+        unique:[true, "Account Already exists with this email address "],
+        required:true,
+    },
+
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
