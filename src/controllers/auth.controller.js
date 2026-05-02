179fe9899bff84c12537aebac1f947006eeeae19
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-05-03 01:37:50 +0530

    controller for user

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const userModel = require("../models/user.model")
+
+/**
+ * @name registerUserController
+ * @description Controller to handle user registration
+ * @access Public
+ */
+
+async function registerUserController(req, res){}
+
+module.exports = {
+    registerUserController
+}
