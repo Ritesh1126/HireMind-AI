4530e4ea9baef3c3fdae61032be766bcc20e2412
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-05-03 01:38:18 +0530

    routes and api are created

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,3 +1,13 @@
 const express = require('express')
+const userModel = require("../models/user.model")
 
-const authRouter = express.Router()
+const authRouter = Router()
+/**
+ * @route POST /api/auth/register
+ * @description Register new user
+ * @access Public
+ */
+
+authRouter.post("/register", authController.registerUserController)
+
+module.exports = authRouter
