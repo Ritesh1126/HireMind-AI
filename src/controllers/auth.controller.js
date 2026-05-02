1cc17808e9e5bb38f7fd2521ded7d84d20cf3efc
Ritesh Sontakke <sontakkeritesh0@gmail.com>
2026-05-03 01:46:51 +0530

    login user controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,7 +6,23 @@ const userModel = require("../models/user.model")
  * @access Public
  */
 
-async function registerUserController(req, res){}
+async function registerUserController(req, res){
+    const {username, email, password} = req.body
+
+    if(!username || !email || !password){
+        return res.status(400).json({message: "All fields are required"})
+    }
+
+    const isUserAlreadyExist = await userModel.findOne({
+        $or:[{username}, {email}]
+    })
+
+    if(isUserAlreadyExist){
+        return res.status(400).json({message: "User already exists"})
+    }
+
+    // Continue with user creation logic
+}
 
 module.exports = {
     registerUserController
